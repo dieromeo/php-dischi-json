c08de44af52bb8e8079c3baaf812574552cd9731
dieromeo <diegoromeo99@icloud.com>
2024-01-11 15:58:37 +0100

    feat: dati in file json

--- script.php
+++ script.php
@@ -1,1 +1,6 @@
 <?php
+// salvataggio dati
+$database_album = json_decode(file_get_contents(__DIR__ . '/album.json'));
+
+header('Content-Type: application/json');
+echo json_encode($database_album);
--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,17 @@ createApp({
         return {
             dischi: []
         }
+    },
+    methods: {
+        // chiamata axios
+        getAlbum() {
+            axios.get('script.php').then((response) => {
+                this.dischi = response.data;
+                console.log(this.dischi)
+            })
+        },
+    },
+    created() {
+        this.getAlbum()
     }
 }).mount('#app');
